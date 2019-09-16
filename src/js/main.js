d6b87822fe3fd90fc4904af6045db26191e4f0ad
Brent Hsieh <horus882@gmail.com>
2019-09-16 12:41:30 +0800

    加入 MiniCssExtractPlugin

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 /* src/js/main.js －　寫一些 jQuery 效果 */
 
-import '../css/style.css';
-import '../css/buttons.css';
+// import '../css/style.css'; //本回合未使用
+// import '../css/buttons.css'; //本回合未使用
+import '../scss/_colors.scss'; // 加入潮潮的scss
+import '../scss/style.scss'; // 加入潮潮的scss
+import '../scss/buttons.scss'; // 加入潮潮的scss
 
 import {myButton, myDesc} from './init';
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,21 @@
  
 var path    = require('path');
 var webpack = require('webpack');
+
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin'); // Webpack 4 [webpack --mode production] 已會自動壓縮了，不太需要這邊僅作練習
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // Webpack 4 改用這個
+
+/* Webpack 4 不支援
+
+var ExtractTextPlugin = require('extract-text-webpack-plugin'); // 使用 extract text webpack plugin
+var extractPlugin = new ExtractTextPlugin({ // 建立一個 extract text plugin 的實例
+    filename: 'bundle.css' // scss轉css後另存的目標檔名
+});
+
+*/
  
 module.exports = {
-    mode: 'development',
+    mode: 'development', // development, production
     entry: './src/js/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -18,12 +30,44 @@ module.exports = {
                     'style-loader',     // 這個會後執行 (順序很重要)
                     'css-loader'        // 這個會先執行
                 ]
+            },
+            {
+                test: /\.(scss|sass)$/,
+                use: [
+                    // 需要用到的 loader
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ]
             }
         ]
     },
+    optimization: {
+        minimizer: [
+            // we specify a custom UglifyJsPlugin here to get source maps in production
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                uglifyOptions: {
+                    compress: false,
+                    ecma: 6,
+                    mangle: true
+                },
+                sourceMap: true
+            })
+        ]
+    },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin(function(){
-            // 這裡應該可以設定一些東西，但不是本篇想討論的，有興趣可以 Google 這個 plugin 可以做啥...
-        }),
+        // new webpack.optimize.UglifyJsPlugin(function(){
+        //     // 這裡應該可以設定一些東西，但不是本篇想討論的，有興趣可以 Google 這個 plugin 可以做啥...
+        // }),
+        // extractPlugin // 把extract過的loader轉存成css檔
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // all options are optional
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+            ignoreOrder: false, // Enable to remove warnings about conflicting order
+        })
     ]
 }
