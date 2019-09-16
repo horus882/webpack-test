62bb19c826d84f6413e1b3f86374cf6cf4c379d4
Brent Hsieh <horus882@gmail.com>
2019-09-16 12:57:14 +0800

    修改 webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -24,13 +24,13 @@ module.exports = {
     },
     module: {
         rules: [
-            {
-                test: /\.css$/,         // 針對所有.css 的檔案作預處理，這邊是用 regular express 的格式
-                use: [
-                    'style-loader',     // 這個會後執行 (順序很重要)
-                    'css-loader'        // 這個會先執行
-                ]
-            },
+            // {
+            //     test: /\.css$/,         // 針對所有.css 的檔案作預處理，這邊是用 regular express 的格式
+            //     use: [
+            //         'style-loader',     // 這個會後執行 (順序很重要)
+            //         'css-loader'        // 這個會先執行
+            //     ]
+            // },
             {
                 test: /\.(scss|sass)$/,
                 use: [
